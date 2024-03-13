4dc988d0e794f2f3c7037e175a25c1490fae8c22
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2024-03-13 03:28:57 +0100

    Update github.service.ts backend url

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -10,7 +10,7 @@ export class GithubService {
   public retourListeUsers$: Subject<any[]> = new Subject();
   public retourListeSearch$: Subject<any> = new Subject();
   public retourDetailUser$: Subject<SingleUser> = new Subject();
-  public route = 'https://flask-service.utbmilcuvav7a.us-west-2.cs.amazonlightsail.com/users';
+  public route = 'https://github-user-stats.onrender.com/users';
   public messageErreur$: Subject<any> = new Subject();
   public query: Map<string, string[]> = new Map<string, string[]>();
 
