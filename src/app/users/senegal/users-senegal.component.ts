9d8e69cf85c00564d154df4e555af169b3142b51
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2024-06-10 16:46:21 +0000

    fix: remove api detail call

--- src/app/users/senegal/users-senegal.component.ts
+++ src/app/users/senegal/users-senegal.component.ts
@@ -22,7 +22,6 @@ export class UsersSenegalComponent implements OnInit, OnDestroy {
   constructor(private githubService: GithubService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.githubService.obtenirDetailUser('bambadiagne');
     this.githubService.obtenirContributionsSenegal();
     this.suscriptions.push(
       this.githubService.retourListeUsers$.subscribe((result: any) => {
